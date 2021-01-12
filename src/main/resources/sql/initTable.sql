8ae019dd6e32e7900d6247ab512b1ff946180159
hanghang <wto8559@163.com>
2021-01-12 17:20:52 +0800

    基础功能搭建

---
+++ src/main/resources/sql/initTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `init_table1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `sex` int(2) DEFAULT NULL,
+  `course_id` int(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8
